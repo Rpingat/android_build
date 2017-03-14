4db74fe7ace2cd7b570131dcb8a5b0be10a548ec
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-03-14 18:40:14 -0300

    Add raise_error param on GetBuildProp method Some devices may not have a specific parameter, so don't let the build fails

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -775,7 +775,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     density = GetBuildProp("ro.sf.lcd_density", OPTIONS.info_dict)
     device = GetBuildProp("ro.aim.device", OPTIONS.info_dict)
     androidver = GetBuildProp("ro.build.version.release", OPTIONS.info_dict)
-    manifacturer = GetBuildProp("ro.product.manufacturer", OPTIONS.info_dict)
+    manufacturer = GetBuildProp("ro.product.manufacturer", OPTIONS.info_dict)
     sdkver = GetBuildProp("ro.build.version.sdk", OPTIONS.info_dict)
     script.Print(" **************** Software *****************");
     script.Print(" OS version: %s"%(buildid));
@@ -794,7 +794,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.Print(" **************** Hardware *****************");
     script.Print(" Device codename: %s"%(device));
     script.Print("");
-    script.Print(" Manufacturer: %s"%(manifacturer));
+    script.Print(" Manufacturer: %s"%(manufacturer));
     script.Print("");
     script.Print(" LCD density: %s"%(density));
     script.Print("");
@@ -943,12 +943,15 @@ def LoadPartitionFiles(z, partition):
   return out
 
 
-def GetBuildProp(prop, info_dict):
+def GetBuildProp(prop, info_dict, raise_error=True):
   """Return the fingerprint of the build of a given target-files info_dict."""
   try:
     return info_dict.get("build.prop", {})[prop]
   except KeyError:
-    raise common.ExternalError("couldn't find %s in build.prop" % (prop,))
+    if raise_error:
+      raise common.ExternalError("couldn't find %s in build.prop" % (prop,))
+    else:
+      return "Unknow"
 
 
 def AddToKnownPaths(filename, known_paths):
